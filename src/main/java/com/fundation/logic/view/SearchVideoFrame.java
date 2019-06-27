24a53e2d1a0fd5fae02db9e9a64bc3406c6a0538
John Salazar Pinto <JohnSalazar@fundacion-jala.local>
2019-06-27 16:48:04 -0400

    White search video Frame created,methods  init component and setting implemented

--- src/main/java/com/fundation/logic/view/SearchVideoFrame.java
+++ src/main/java/com/fundation/logic/view/SearchVideoFrame.java
@@ -8,11 +8,46 @@
  */
 package com.fundation.logic.view;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
 /**
  * Implements the SearchVideoFrame class
  *
  * @author John Salazar Pinto
  * @version 1.0
  */
-public class SearchVideoFrame {
+public class SearchVideoFrame extends JFrame {
+
+    private JPanel contentSearchVideoPane;
+
+    public  SearchVideoFrame(){
+
+    initComponent();
+    initSetting();
+
+    }
+
+    /**
+     *This method init all component
+     */
+    public void initComponent() {
+
+        contentSearchVideoPane = new JPanel();
+
+    }
+
+    /**
+     * This method init all setting of the components
+     */
+    public void initSetting() {
+
+        setBounds(100, 100, 1000, 700);
+        contentSearchVideoPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentSearchVideoPane.setLayout(new BorderLayout(0, 0));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(contentSearchVideoPane);
+
+    }
 }
