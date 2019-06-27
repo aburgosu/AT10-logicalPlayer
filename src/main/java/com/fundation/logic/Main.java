fdc334d32ce64d643c4595073eca7d36b8a2a458
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-06-26 21:03:08 -0400

    Main and Criteria classes added.

---
+++ src/main/java/com/fundation/logic/Main.java
@@ -0,0 +1,22 @@
+ /*
+  * Copyright (c) 2019 Jalasoft.
+  * This software is the confidential and proprietary information of Jalasoft.
+  * ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Jalasoft.
+  */
+
+ package com.fundation.logic;
+
+ /* Implements the model class File and the getter and setter´s methods
+  * @author Andres Burgos
+  * @version 1.0 *
+  */
+
+public class Main {
+
+    public static void main(String[] arg){
+
+    }
+}
---
+++ src/main/java/com/fundation/logic/Model/Criteria.java
@@ -0,0 +1,45 @@
+ /*
+  * Copyright (c) 2019 Jalasoft.
+  * This software is the confidential and proprietary information of Jalasoft.
+  * ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Jalasoft.
+  */
+
+ package com.fundation.logic.Model;
+
+  /* Implements the model class File and the getter and setter´s methods
+  * @author Andres Burgos
+  * @version 1.0 *
+  */
+
+ public class Criteria {
+     private String path;
+     private String fileName;
+     private String extension;
+
+     public String getPath() {
+         return path;
+     }
+
+     public void setPath(String path) {
+         this.path = path;
+     }
+
+     public String getFileName() {
+         return fileName;
+     }
+
+     public void setFileName(String fileName) {
+         this.fileName = fileName;
+     }
+
+     public String getExtension() {
+         return extension;
+     }
+
+     public void setExtension(String extension) {
+         this.extension = extension;
+     }
+ }
