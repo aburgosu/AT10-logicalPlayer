59f46ad671616a3af0efc48cc4f683d88e079dd1
johnpiterzon <johnpiterzon@gmail.com>
2019-06-27 22:01:27 -0400

    Size comboBox,Cordenades x for order on columns,creation and modification date fields Implemented

--- src/main/java/com/fundation/logic/view/SearchVideoPanel.java
+++ src/main/java/com/fundation/logic/view/SearchVideoPanel.java
@@ -8,8 +8,9 @@
  */
 package com.fundation.logic.view;
 
-import javax.swing.JPanel;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -20,8 +21,10 @@ import javax.swing.JTextField;
  */
 public class SearchVideoPanel extends JPanel {
 
-    private JTextField txtFldPath, txtFldFileName, txtFldSize;
-    JLabel lblSearch, lblPath, lblFileName, lblSize;
+    private JTextField txtFldPath, txtFldFileName, txtFldSize, txtFldCreationDate, txtFldModificationDate;
+    private JLabel lblSearch, lblPath, lblFileName, lblSize, lblCreationDate, lblModificationDate;
+    private JComboBox comboSize;
+    private Integer positionOneX,positionTwoX,positionThreeX;//position x default
 
     public SearchVideoPanel() {
         initComponent();
@@ -44,6 +47,12 @@ public class SearchVideoPanel extends JPanel {
         add(lblFileName);
         lblSize = new JLabel("Size");
         add(lblSize);
+        lblCreationDate = new JLabel("Creation Date");
+        add(lblCreationDate);
+        lblModificationDate = new JLabel("Modification Date");
+        add(lblModificationDate);
+        //LabelsVideo search options
+
 
         //Init and adds Texts Fields
         txtFldPath = new JTextField();
@@ -52,6 +61,14 @@ public class SearchVideoPanel extends JPanel {
         add(txtFldFileName);
         txtFldSize = new JTextField();
         add(txtFldSize);
+        txtFldCreationDate = new JTextField();
+        add(txtFldCreationDate);
+        txtFldModificationDate = new JTextField();
+        add(txtFldModificationDate);
+
+        //Init and adds combo box
+        comboSize = new JComboBox();
+        add(comboSize);
 
     }
 
@@ -60,18 +77,34 @@ public class SearchVideoPanel extends JPanel {
      */
     public void initSetting() {
 
-        lblSearch.setBounds(24, 23, 62, 27);
-        lblPath.setBounds(10, 56, 51, 27);
-        lblFileName.setBounds(10, 86, 90, 27);
-        lblSize.setBounds(10, 120, 20, 20);
+        positionOneX = 10;
+        positionTwoX = 180;
+        positionThreeX = 300;
 
-        txtFldPath.setBounds(50, 59, 86, 20);
-        txtFldFileName.setBounds(90, 90, 86, 20);
-        txtFldSize.setBounds(90, 120, 86, 20);
+        lblSearch.setBounds(positionOneX, 23, 62, 27);
+        lblPath.setBounds(positionOneX, 56, 51, 27);
+        lblFileName.setBounds(positionOneX, 86, 90, 27);
+        lblSize.setBounds(positionOneX, 120, 40, 20);
+        lblCreationDate.setBounds(positionOneX, 150, 160, 20);
+        lblModificationDate.setBounds(10, 180, 160, 20);
+
+        txtFldPath.setBounds(positionTwoX, 59, 86, 20);
+        txtFldFileName.setBounds(positionTwoX, 90, 86, 20);
+        txtFldSize.setBounds(positionTwoX, 120, 86, 20);
+        comboSize.setBounds(positionThreeX, 120, 45, 20);
+        txtFldCreationDate.setBounds(positionTwoX,150,60,20);
+        txtFldModificationDate.setBounds(positionTwoX,180,60,20);
 
         txtFldPath.setText("...");
         txtFldFileName.setText("...");
         txtFldSize.setText("...");
+        txtFldCreationDate.setText("...");
+        txtFldModificationDate.setText("...");
+
+        //Video search options
+
+
+
 
     }
 }
