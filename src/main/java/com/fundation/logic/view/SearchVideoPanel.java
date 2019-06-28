7079c91aa546a7fbe7b68eafc8d6b71c4ada1667
johnpiterzon <johnpiterzon@gmail.com>
2019-06-27 21:03:34 -0400

    Implement visual parts FileName and Size with their text fields and add comments Init Labels and Init Text Fields

--- src/main/java/com/fundation/logic/view/SearchVideoPanel.java
+++ src/main/java/com/fundation/logic/view/SearchVideoPanel.java
@@ -20,8 +20,8 @@ import javax.swing.JTextField;
  */
 public class SearchVideoPanel extends JPanel {
 
-    private JTextField txtFldPath;
-    JLabel lblSearch, lblPath;
+    private JTextField txtFldPath, txtFldFileName, txtFldSize;
+    JLabel lblSearch, lblPath, lblFileName, lblSize;
 
     public SearchVideoPanel() {
         initComponent();
@@ -35,14 +35,24 @@ public class SearchVideoPanel extends JPanel {
 
         setLayout(null);
 
+        //Init and adds Labels
         lblSearch = new JLabel("Search");
         add(lblSearch);
-
         lblPath = new JLabel("Path");
         add(lblPath);
+        lblFileName = new JLabel("File Name");
+        add(lblFileName);
+        lblSize = new JLabel("Size");
+        add(lblSize);
 
+        //Init and adds Texts Fields
         txtFldPath = new JTextField();
         add(txtFldPath);
+        txtFldFileName = new JTextField();
+        add(txtFldFileName);
+        txtFldSize = new JTextField();
+        add(txtFldSize);
+
     }
 
     /**
@@ -52,9 +62,16 @@ public class SearchVideoPanel extends JPanel {
 
         lblSearch.setBounds(24, 23, 62, 27);
         lblPath.setBounds(10, 56, 51, 27);
-        lblPath.setBounds(10, 56, 51, 27);
+        lblFileName.setBounds(10, 86, 90, 27);
+        lblSize.setBounds(10, 120, 20, 20);
+
         txtFldPath.setBounds(50, 59, 86, 20);
+        txtFldFileName.setBounds(90, 90, 86, 20);
+        txtFldSize.setBounds(90, 120, 86, 20);
+
         txtFldPath.setText("...");
+        txtFldFileName.setText("...");
+        txtFldSize.setText("...");
 
     }
 }
