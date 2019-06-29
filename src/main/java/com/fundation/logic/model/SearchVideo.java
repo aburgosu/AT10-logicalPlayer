7134e1e692e433489ecf1299deb807eba42793c5
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-06-29 14:51:49 -0400

    Add two funtionality to search file hidden and Read Only

--- src/main/java/com/fundation/logic/model/SearchVideo.java
+++ src/main/java/com/fundation/logic/model/SearchVideo.java
@@ -1,13 +1,14 @@
- /**
-  * Copyright (c) 2019 Jalasoft.
-  * This software is the confidential and proprietary information of Jalasoft.
-  * ("Confidential Information"). You shall not
-  * disclose such Confidential Information and shall use it only in
-  * accordance with the terms of the license agreement you entered into
-  * with Jalasoft.
-  */
+/**
+*
+* * Copyright (c) 2019 Jalasoft.
+* This software is the confidential and proprietary information of Jalasoft.
+* ("Confidential Information"). You shall not
+* disclose such Confidential Information and shall use it only in
+* accordance with the terms of the license agreement you entered into
+* with Jalasoft.
+* */
 
- package com.fundation.logic.Model;
+package com.fundation.logic.model;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -17,22 +18,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
- /** Implements the model class File and the getter and setter´s methods
-  * @author Jesus Menacho
-  * @version 1.0 *
-  */
-public class SearchVideo implements ISearch {
+/** Implements the model class File and the getter and setter´s methods
+* @author Jesus Menacho
+* @version 1.0 *
+*/
+public class SearchVideo implements ISearch{
 static Criteria criteria;
-
     public SearchVideo(Criteria criteria){
         this.criteria = criteria;
     }
-
     public List searchByPath() {
-        if ((criteria.getPath().equals("")) && (criteria.getFileName().equals("")) && (criteria.getExtension().equals(""))) {
-            return null;
-        }
-        if ((criteria.getPath().equals(""))) {
+        if ((criteria.getPath().equals("")) || (criteria.getPath().equals(null))) {
             return null;
         }
         final String FILE_PATH = criteria.getPath();
@@ -40,26 +36,50 @@ static Criteria criteria;
         File file = new File(FILE_PATH);
         File[] allSubFiles = file.listFiles();
         for (File fileExtractor : allSubFiles) {
-            if (!(criteria.getPath().equals("")) && (criteria.getFileName().equals("")) && (criteria.getExtension().equals(""))) {
-                listFileAndDirectory.add(new File(fileExtractor.getAbsolutePath()));
-            }
-            if (!(criteria.getPath().equals("")) && (!criteria.getFileName().equals("")) && (criteria.getExtension().equals(""))) {
-                if (fileExtractor.getName().contains(criteria.getFileName()) && fileExtractor.getAbsolutePath().contains(criteria.getPath())) {
-                    listFileAndDirectory.add(new File(fileExtractor.getAbsolutePath()));
+            if (fileExtractor.isHidden() == criteria.getFileHidden())
+            {
+                if ((fileExtractor.canRead() && !fileExtractor.canWrite()) == criteria.getFileReadOnly())
+                {
+                    listFileAndDirectory.add(compareFile(fileExtractor));
+                }
+                else
+                {
+                    listFileAndDirectory.add(compareFile(fileExtractor));
                 }
             }
-            if (!(criteria.getPath().equals("")) && (!criteria.getFileName().equals("")) && (!criteria.getExtension().equals(""))) {
-                if (fileExtractor.getName().contains(criteria.getFileName()) && fileExtractor.getName().contains(criteria.getExtension())) {
-                    listFileAndDirectory.add(new File(fileExtractor.getAbsolutePath()));
+            else
+            {
+                if ((fileExtractor.canRead() && !fileExtractor.canWrite()) == criteria.getFileReadOnly())
+                {
+                    listFileAndDirectory.add(compareFile(fileExtractor));
                 }
-            }
-            if (!(criteria.getPath().equals("")) && (criteria.getFileName().equals("")) && (!criteria.getExtension().equals(""))) {
-                if (fileExtractor.getName().contains(criteria.getExtension())) {
-                    listFileAndDirectory.add(new File(fileExtractor.getAbsolutePath()));
+                else
+                {
+                    listFileAndDirectory.add(compareFile(fileExtractor));
                 }
             }
         }
         return listFileAndDirectory;
     }
+    private File compareFile(File fileExtractor){
+        if (!(criteria.getPath().equals("")) && (criteria.getFileName().equals("")) && (criteria.getExtension().equals(""))) {
+            return (new File(fileExtractor.getAbsolutePath()));
+        }
+        if (!(criteria.getPath().equals("")) && (!criteria.getFileName().equals("")) && (criteria.getExtension().equals(""))) {
+            if (fileExtractor.getName().contains(criteria.getFileName()) && fileExtractor.getPath().contains(criteria.getPath())) {
+                return (new File(fileExtractor.getAbsolutePath()));
+            }
+        }
+        if (!(criteria.getPath().equals("")) && (!criteria.getFileName().equals("")) && (!criteria.getExtension().equals(""))) {
+            if (fileExtractor.getName().contains(criteria.getFileName()) && fileExtractor.getName().contains(criteria.getExtension())) {
+                return (new File(fileExtractor.getAbsolutePath()));
+            }
+        }
+        if (!(criteria.getPath().equals("")) && (criteria.getFileName().equals("")) && (!criteria.getExtension().equals(""))) {
+            if (fileExtractor.getName().contains(criteria.getExtension())) {
+                return (new File(fileExtractor.getAbsolutePath()));
+            }
+        }
+        return null;
+    }
 }
-
