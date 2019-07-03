c1c11e39110f901c1866524a6d3a59187c945acf
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-07-03 14:32:12 -0400

    Refactor of panels of main JFrame to reorganize

--- src/main/java/com/fundation/logic/view/SearchVideoFrame.java
+++ src/main/java/com/fundation/logic/view/SearchVideoFrame.java
@@ -11,6 +11,8 @@ package com.fundation.logic.view;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 
@@ -21,11 +23,10 @@ import java.awt.BorderLayout;
  * @version 1.0
  */
 public class SearchVideoFrame extends JFrame {
-    private String pathAddress;
+    JPanel mainContentPanel;
     private SearchVideoPanel mainSearchPanel;
 
     public SearchVideoFrame() {
-        initComponent();
         initSetting();
     }
 
@@ -33,23 +34,36 @@ public class SearchVideoFrame extends JFrame {
      * This method init all component
      */
     public void initComponent() {
+
+        mainContentPanel = new JPanel();
+        mainContentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        mainContentPanel.setLayout(new BorderLayout(0, 0));
+
+        JSplitPane mainSplitPanel = new JSplitPane();
+        mainSplitPanel.setDividerLocation(getWidth()/2);
+        mainContentPanel.add(mainSplitPanel, BorderLayout.CENTER);
+
+        TableResult tableResult = new TableResult();
+        JScrollPane tableScrollPanel = new JScrollPane();
+        tableScrollPanel.add(tableResult);
+        mainSplitPanel.setRightComponent(tableResult);
+
+
         mainSearchPanel = new SearchVideoPanel();
-        add(mainSearchPanel);
+        mainSplitPanel.setLeftComponent(mainSearchPanel);
         SearchVideoPanel initSearchPanel = new SearchVideoPanel();
-        contentSearchVideoPane.add(initSearchPanel);
-        setVisible(true);
+
     }
 
     /**
      * This method init all setting of the components like the position
      */
     public void initSetting() {
-        setBounds(100, 100, 700, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public String getPath() {
-        pathAddress = mainSearchPanel.getPath();
-        return pathAddress;
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setBounds(100, 100, 800, 400);
+        initComponent();
+        setContentPane(mainContentPanel);
+        setVisible(true);
     }
 }
