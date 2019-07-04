4fbd9d6b917735fc19c934db8d26414ef89693eb
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-07-04 12:42:43 -0400

    Implement SQL consult insert and show that we have Data base

--- src/main/java/com/fundation/logic/Database/Query.java
+++ src/main/java/com/fundation/logic/Database/Query.java
@@ -9,43 +9,28 @@
  */
 package com.fundation.logic.Database;
 
-/**
- * Implements insert in a table .
- *
- * @author Andres Burgos
- * @version 1.0
- */
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
+/**
+ * Implements insert in a table .
+ *
+ * @author Andres Burgos, Jesus Menacho
+ * @version 1.0
+ */
 public class Query {
 
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:at10-player.db";
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            e.getMessage();
-        }
-        return connection;
-    }
-
     public void insertCriteria(String name, String json) {
-        String sql = "INSERT INTO criterias(name, json) VALUES(?,?,?)";
-
+        String sql = "INSERT INTO criterias(name, date, json) VALUES(?,?,?)";
         try {
-            Connection connection = this.connect();
+            Connection connection =DBConnection.initConnection();
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, name);
-            statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+            statement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
             statement.setString(3, json);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -56,15 +41,18 @@ public class Query {
     public void selectAllCriterias() {
         String sql = "SELECT * FROM criterias";
         try {
-            Connection conn = this.connect();
+            Connection conn = DBConnection.initConnection();
             Statement statement = conn.createStatement();
             ResultSet result = statement.executeQuery(sql);
-            // loop through the result set
             while (result.next()) {
-                System.out.println(result.getInt("id") + "\t" + result.getString("name") + "\t" + result.getDouble("capacity"));
+                System.out.println(result.getInt("name") + "\t" + result.getString("date") + "\t" + result.getDouble("json"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public static void main(String arg[]){
+        Query query=new Query();
+        query.selectAllCriterias();
     }
 }
