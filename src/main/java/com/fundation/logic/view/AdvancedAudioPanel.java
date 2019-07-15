a2d6dbba92f9dff8cb90c93123a996a076a794e0
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-07-15 10:54:46 -0400

    Refactor position the combobox

--- src/main/java/com/fundation/logic/view/AdvancedAudioPanel.java
+++ src/main/java/com/fundation/logic/view/AdvancedAudioPanel.java
@@ -132,7 +132,8 @@ public class AdvancedAudioPanel extends JPanel {
 
         btnSearchAdvanceAudio = new JButton("Search");
         GridBagConstraints gbc_btnSearchAdvanceAudio = new GridBagConstraints();
-        gbc_btnSearchAdvanceAudio.gridwidth = 2;
+        gbc_btnSearchAdvanceAudio.anchor = GridBagConstraints.EAST;
+        //gbc_btnSearchAdvanceAudio.gridwidth = 2;
         gbc_btnSearchAdvanceAudio.gridx = 1;
         gbc_btnSearchAdvanceAudio.gridy = 12;
         add(btnSearchAdvanceAudio, gbc_btnSearchAdvanceAudio);
