1687ed0cad0b02e273fd8c2bb151e2737e979874
johnpiterzon <johnpiterzon@gmail.com>
2019-07-22 01:38:11 -0400

    init exiftool (run), readAll metadata, find channel find audio codec implemented

--- src/main/java/com/fundation/logic/common/MetadataAudioExtractor.java
+++ src/main/java/com/fundation/logic/common/MetadataAudioExtractor.java
@@ -21,30 +21,99 @@ import java.io.InputStreamReader;
  * @version 1.0
  */
 public class MetadataAudioExtractor {
+    private static Process extractMetadata;
+    private static String searchChannelMode;
+    private static String searchAudioCodec;
+    private static String searchMimeType;
+
+    public void run(String path) throws IOException {
+        extractMetadata = Runtime.getRuntime().exec(path);
+        readAll();
+    }
+
+    /**
+     * This method read all metadata.
+     *
+     * @return
+     */
+    public String readAll() {
+        String frameRate = null;
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(extractMetadata.getInputStream()));
+        try {
+            while ((frameRate = stdInput.readLine()) != null) {
+                String s = frameRate;
+                audioChannel(s);
+                audioCodec(s);
+                mimeType(s);
+                if ((frameRate.contains("Read_All"))) {
+                    int initIndex = frameRate.indexOf(":");
+                    int endIndex = frameRate.length();
+                    int freeSpace = 2;
+                    frameRate = frameRate.substring(initIndex + freeSpace, endIndex);
+                    frameRate = Float.toString(Math.round(Float.parseFloat(frameRate)));
+                    int start = 0;
+                    int deleteDat0 = 2;
+                    frameRate = frameRate.substring(start, deleteDat0);
+                    return frameRate;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+            System.out.println("No paso frame rate");
+        }
+        return "NO hay";
+    }
 
     /**
      * This method returns duration of the audio.
      */
-    public static String getAudioChannel(File pathFile) {
-        String channelMode = null;
-        try {
-            Process extractMetadata = Runtime.getRuntime().exec("thirdParty/exiftool.exe " + pathFile.toString());
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(extractMetadata.getInputStream()));
-            while ((stdInput.readLine()) != null) {
-                channelMode = stdInput.readLine();
-                if ((channelMode.contains("Lame Stereo Mode"))) {
-                    int initIndex = channelMode.indexOf(":");
-                    int endIndex = channelMode.length();
-                    int freeSpace = 2;
-                    channelMode = channelMode.substring(initIndex + freeSpace, endIndex);
-                    return channelMode;
-                }
+    public static void audioChannel(String channelMode) {
+        if ((channelMode.contains("Channel"))) {
+            if (channelMode.contains("Stereo")){
+                searchChannelMode = "2";
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            System.exit(-1);
+            if (channelMode.contains("Mono")){
+                searchChannelMode = "1";
+            }
         }
-        return "there is not audio channel";
+    }
+
+    public static String getAudioChannel(){
+        return searchChannelMode;
+    }
+
+    /**
+     * This method returns duration of the audio.
+     */
+    public static void audioCodec(String audioCodec) {
+        if ((audioCodec.contains("MIME Type"))) {
+            if (audioCodec.contains("mpeg")){
+                searchAudioCodec = "MPEG-4 ALS";
+            }
+            if (audioCodec.contains("wav")){
+                searchAudioCodec = "WAV";
+            }
+            if (audioCodec.contains("mp3")){
+                searchChannelMode = "MP3";
+            }
+        }
+    }
+
+    public static String searchAudioCodec(){
+        return searchAudioCodec;
+    }
+
+    public static void mimeType(String mimeType) {
+        if ((mimeType.contains("MIME Type"))) {
+            if (mimeType.contains("audio")) {
+                searchMimeType = "audio";
+            }
+        }
+    }
+
+    public static String searchMimeType(){
+        return searchMimeType;
     }
 
     /**
