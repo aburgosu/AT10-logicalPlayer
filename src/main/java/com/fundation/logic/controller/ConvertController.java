b0e59e72daed8d6afa9452e42baa57055f3a6f29
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-07-26 23:53:12 -0400

    Reset criteria after convert

--- src/main/java/com/fundation/logic/controller/ConvertController.java
+++ src/main/java/com/fundation/logic/controller/ConvertController.java
@@ -64,6 +64,7 @@ public class ConvertController {
             .addActionListener(e -> {
                 ConvertCriteria criteria = setConvertCriteria("PDF");
             });
+        resetCriteria();
     }
 
     /**
