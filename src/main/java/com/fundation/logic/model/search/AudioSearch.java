9f15c5a056b0fa5fa3fbbe688c116b19cc6e52ba
johnpiterzon <johnpiterzon@gmail.com>
2019-07-27 04:07:35 -0400

    duration , convert duration to decimal criteria of evaluation duration fixed

--- src/main/java/com/fundation/logic/model/search/AudioSearch.java
+++ src/main/java/com/fundation/logic/model/search/AudioSearch.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2019 Jalasoft.
- * <p>
+ *
  * This software is the confidential and proprietary information of Jalasoft.
  * ("Confidential Information"). You shall not
  * disclose such Confidential Information and shall use it only in
@@ -11,8 +11,6 @@ package com.fundation.logic.model.search;
 
 import com.fundation.logic.common.FileInfo;
 import com.fundation.logic.common.MetadataAudioExtractor;
-import com.fundation.logic.common.MetadataCommonExtractor;
-import com.fundation.logic.common.MetadataImageExtractor;
 import com.fundation.logic.model.CustomizedFile;
 import com.fundation.logic.model.searchCriteria.Audio;
 
@@ -22,7 +20,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Implements new Video search class.
+ * Implements new Audio search class.
  *
  * @author John Salazar Pinto
  * @version 1.0
@@ -31,7 +29,7 @@ public class AudioSearch implements ISearch {
     static private Audio audioCriteria;
 
     /**
-     * Initializes a search instance which requires a searchCriteria as parameter.
+     * Initializes a search instance which requires a criteria as parameter.
      */
     public AudioSearch(Audio audioCriteria) {
         this.audioCriteria = audioCriteria;
@@ -40,7 +38,7 @@ public class AudioSearch implements ISearch {
     /**
      * Main search method.
      *
-     * @return List of found items if searchCriteria's path is not null.
+     * @return List of found items if criteria's path is not null.
      */
     public List search() {
         if (audioCriteria.getPath() == null) {
@@ -51,7 +49,7 @@ public class AudioSearch implements ISearch {
 
     /**
      * @param path
-     * @return Complete list of found items according on searchCriteria's path.
+     * @return Complete list of found items according on criteria's path.
      */
     public List searchInPath(String path) {
         List<CustomizedFile> searchResult = new ArrayList<>();
@@ -59,11 +57,13 @@ public class AudioSearch implements ISearch {
         String criteriaFileName = audioCriteria.getFileName();
         String criteriaExtension = audioCriteria.getExtension();
         String criteriAudioCodec = audioCriteria.getAudioCodec();
-        String criteriaDuration = Integer.toString(audioCriteria.getDuration());
-        String criteriaChannel = Integer.toString(audioCriteria.getChannel());
-        String criteriaMimeType = "audio";
-        String criteriaSampleRate = Integer.toString(audioCriteria.getSampleRate());
+        String criteriaLLDuration = audioCriteria.getDurationfrom();
+        String criteriaLUDuration = audioCriteria.getDurationTo();
+        String criteriaChannel = audioCriteria.getChannel();
+        String criteriaSampleRate = audioCriteria.getSampleRate();
         File[] allSubFiles = file.listFiles();
+        Float initDuration = convertDurationToDecimal(criteriaLLDuration);
+        Float endDuration = convertDurationToDecimal(criteriaLUDuration);
         for (File fileExtractor : allSubFiles) {
             try {
                 if (fileExtractor.isDirectory()) {
@@ -71,12 +71,11 @@ public class AudioSearch implements ISearch {
                 } else {
                     String fileName = FileInfo.getFileDenomination(fileExtractor, "name");
                     String fileExtension = FileInfo.getFileDenomination(fileExtractor, "extension");
-                    MetadataImageExtractor metadataImageExtractor = new MetadataImageExtractor();
+                    String owner = FileInfo.getFileOwner(fileExtractor, "user");
                     String exiftool = "thirdParty/exiftool.exe "; //Tool used for extract metadata
-                    String pathd = exiftool + "\"" + fileExtractor + "\"";
+                    String filePath = exiftool + "\"" + fileExtractor + "\"";
                     MetadataAudioExtractor metadataAudioExtractor = new MetadataAudioExtractor();
-                    metadataAudioExtractor.run(pathd);
-                    metadataImageExtractor.run(pathd);
+                    metadataAudioExtractor.run(filePath);
                     String channel = "All";
                     if (criteriaChannel != "All") {
                         channel = MetadataAudioExtractor.getAudioChannel();
@@ -85,40 +84,41 @@ public class AudioSearch implements ISearch {
                     if (criteriAudioCodec != "All") {
                         audioCodec = MetadataAudioExtractor.searchAudioCodec();
                     }
-                    String mimeType = MetadataAudioExtractor.searchMimeType();
-
                     String sampleRate = "All";
                     if (criteriaSampleRate != "All") {
                         sampleRate = MetadataAudioExtractor.getSearchSampleRate();
                     }
+                    if (initDuration == 0.0 && endDuration == 0.0) {
+                        initDuration = new Float(0.0);
+                        endDuration = new Float(12.9);
+                    }
+                    Float duration = MetadataAudioExtractor.getSearchDuration();
                     Date creationDate = FileInfo.getFileDate(fileExtractor, "creation");
                     Date accessDate = FileInfo.getFileDate(fileExtractor, "access");
                     Date modificationDate = FileInfo.getFileDate(fileExtractor, "modification");
                     Float fileSize = FileInfo.getFileSize(fileExtractor);
-
                     if (evaluateString(fileName, criteriaFileName)
                             && evaluateString(fileExtension, criteriaExtension)
                             && evaluateString(channel, criteriaChannel)
                             && evaluateString(audioCodec, criteriAudioCodec)
-                            && evaluateString(mimeType, criteriaMimeType)
-                            && evaluateString(sampleRate, criteriaSampleRate)) {
-                        List<String> metadata = MetadataCommonExtractor.getSearchListMetadata();
+                            && evaluateString(sampleRate, criteriaSampleRate)
+                            && evaluateDuration(duration, initDuration, endDuration)) {
+                        List<String> metadata = MetadataAudioExtractor.getSearchListMetadata();
                         CustomizedFile matchingFile = new CustomizedFile(fileExtractor.getAbsolutePath(), fileName,
                                 fileExtension, false, false,
                                 fileSize, creationDate, accessDate,
-                                modificationDate, "MimeType", "video", metadata);
+                                modificationDate, owner, "audio", metadata);
                         searchResult.add(matchingFile);
                     }
                 }
             } catch (Exception e) {
-                System.out.println("The file  :" + FileInfo.getFileDenomination(fileExtractor, "name") + " -  No was added ");
             }
         }
         return searchResult;
     }
 
     /**
-     * Evaluates specific string field according on searchCriteria.
+     * Evaluates specific string field according on criteria.
      *
      * @return Answer after evaluation.
      */
@@ -127,5 +127,38 @@ public class AudioSearch implements ISearch {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Convert specific duration to decimal.
+     *
+     * @return decimal duration.
+     */
+    public Float convertDurationToDecimal(String duration) {
+        Float hourToSeconds = new Float(3600);
+        Float minuteToSeconds = new Float(60);
+        Float hour = Float.parseFloat(duration.substring(0, 2)) * hourToSeconds;
+        Float minutes = Float.parseFloat(duration.substring(3, 5)) * minuteToSeconds;
+        Float seconds = Float.parseFloat(duration.substring(6, 8));
+        Float initDuration = (hour + minutes + seconds) / 3600;
+        return initDuration;
+    }
+
+    /**
+     * Evaluates if the duration size is between set limits.
+     *
+     * @return Answer after evaluation.
+     */
+    private boolean evaluateDuration(Float metadataDuration, Float lowerLimit, Float upperLimit) {
+        if (lowerLimit == null && upperLimit == null) {
+            return true;
+        }
+        if (lowerLimit != null && upperLimit == null) {
+            return metadataDuration >= lowerLimit;
+        }
+        if (lowerLimit == null && upperLimit != null) {
+            return metadataDuration <= upperLimit;
+        }
+        return (metadataDuration >= lowerLimit && metadataDuration <= upperLimit);
     }
 }
