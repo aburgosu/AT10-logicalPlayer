73e5b8174d8877cc7e784cd4f61db9580b4dc992
MelissaRoman06 <Melissa.Roman@fundacion-jala.org>
2019-07-29 18:47:11 -0400

    Unnecessary comments deleted

--- src/main/java/com/fundation/logic/view/loadSaveCriteria/PopupLoadSave.java
+++ src/main/java/com/fundation/logic/view/loadSaveCriteria/PopupLoadSave.java
@@ -11,9 +11,6 @@ package com.fundation.logic.view.loadSaveCriteria;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.List;
 
 /**
  * Implements PopupLoadSave class which is in charge of delete or load criteria.
@@ -33,7 +30,6 @@ public class PopupLoadSave extends JPopupMenu {
         add(loadItem);
         deleteItem = new JMenuItem("Delete");
         add(deleteItem);
-        //initItemMenuListener();
     }
 
     /**
@@ -51,38 +47,4 @@ public class PopupLoadSave extends JPopupMenu {
     public JMenuItem getDeleteItem() {
         return deleteItem;
     }
-/**
-     * Initializes item menu listener.
-     */
-    /*
-    public void initItemMenuListener(){
-        try {
-            loadItem.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent event) {
-                    if (event.getButton() == MouseEvent.BUTTON1) {
-                        PlayerFrame playerWindow = new PlayerFrame(filePath);
-                        playerWindow.setVisible(true);
-                    }
-                }
-            });
-        } catch (Exception exception) {
-            exception.getMessage();
-        }
-
-        try {
-            deleteItem.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent event) {
-                    if (event.getButton() == MouseEvent.BUTTON1) {
-                        DetailsFrame details = new DetailsFrame(filePath, detailsList);
-                        details.setVisible(true);
-                    }
-                }
-            });
-        } catch (Exception exception) {
-            exception.getMessage();
-        }
-    }
-    */
 }
