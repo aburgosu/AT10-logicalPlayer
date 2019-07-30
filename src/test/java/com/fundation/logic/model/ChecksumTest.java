7ed424162e47cc3be4faa8c5a25b96e21917f63d
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-07-30 13:48:33 -0400

    Checksum unit test added

---
+++ src/test/java/com/fundation/logic/model/ChecksumTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+package com.fundation.logic.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This class serves as unit test class for checksum class.
+ *
+ * @author Andres
+ * @version 1.0
+ */
+public class ChecksumTest {
+    @Test
+    public void getChecksum() throws Exception {
+        String filename = "resources/Research Data Types.pdf";
+        String algorithm = "MD5";
+        String actual = Checksum.getChecksum(filename, algorithm);
+        String expected = "42c0e7c9c36230a917b30ea942cdd0ba";
+        assertEquals(expected, actual);
+    }
+}
