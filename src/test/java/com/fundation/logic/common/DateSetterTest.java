3c7229ded2248c564358e643feb56b36de04acab
Maday Alcala <maday.alcala@fundacion-jala.org>
2019-07-30 22:20:09 -0400

    Common data setter Test added.

--- src/test/java/com/fundation/logic/common/DateSetterTest.java
+++ src/test/java/com/fundation/logic/common/DateSetterTest.java
@@ -3,33 +3,44 @@ package com.fundation.logic.common;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
+
+import static java.util.Calendar.HOUR_OF_DAY;
 
 public class DateSetterTest {
+
+    /**
+     * This unit test verify the setStartOfDay.
+     */
     @Test
     public void DateStarTest(){
-        Date fato = new Date(2019, 05,02, 05, 20, 2222);
-        Date expected = new Date(2019, 05,02, 00, 00, 00);
-        Date actual = DateSetter.setStartOfDay(fato);
-        Assert.assertEquals(expected,actual);
-    }
-    @Test
-    public void FinalStarTest(){
-        Date fato = new Date(2019, 05,02, 05, 20, 20);
-        GregorianCalendar cal;
-        cal = new GregorianCalendar();
-        // discard time of day so we only have the date
-        cal.set(Calendar.HOUR_OF_DAY, cal.getActualMinimum(23));
-        cal.set(Calendar.MINUTE, cal.getActualMinimum(59));
-        cal.set(Calendar.SECOND, cal.getActualMinimum(59));
-        cal.set(Calendar.MILLISECOND, cal.getActualMinimum(999));
-        Date format = new Date(String.valueOf(cal.getTime()));
-        Date expected = new Date(String.valueOf(format));
-        Date actual = DateSetter.setStartOfDay(fato);
+        Date dateStart = new Date(2019, 05,02, 05, 20, 22);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dateStart);
+        cal.set(HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        Date expected = cal.getTime();
+        Date actual = DateSetter.setStartOfDay(dateStart);
         Assert.assertEquals(expected,actual);
     }
 
+    /**
+     * This unit test verify the setEndOfDay.
+     */
+    @Test
+    public void EndOfDayTest(){
+        Date dateEnd = new Date(2019, 05,02, 05, 20, 22);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dateEnd);
+        cal.set(HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        Date expected = cal.getTime();
+        Date actual = DateSetter.setEndOfDay(dateEnd);
+        Assert.assertEquals(expected,actual);
+    }
 }
