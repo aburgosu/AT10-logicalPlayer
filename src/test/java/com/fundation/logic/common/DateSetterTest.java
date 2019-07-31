fae8be69152ba0f9c5cc8175f8fd6562a41655d6
MadayAlcala <49444782+MadayAlcala@users.noreply.github.com>
2019-07-31 08:47:27 -0400

    Adding spaces.

--- src/test/java/com/fundation/logic/common/DateSetterTest.java
+++ src/test/java/com/fundation/logic/common/DateSetterTest.java
@@ -30,7 +30,7 @@ public class DateSetterTest {
      */
     @Test
     public void DateStarTest(){
-        Date dateStart = new Date(2019, 05,02, 05, 20, 22);
+        Date dateStart = new Date(2019, 05, 02, 05, 20, 22);
         Calendar cal = Calendar.getInstance();
         cal.setTime(dateStart);
         cal.set(HOUR_OF_DAY, 0);
@@ -47,7 +47,7 @@ public class DateSetterTest {
      */
     @Test
     public void EndOfDayTest(){
-        Date dateEnd = new Date(2019, 05,02, 05, 20, 22);
+        Date dateEnd = new Date(2019, 05, 02, 05, 20, 22);
         Calendar cal = Calendar.getInstance();
         cal.setTime(dateEnd);
         cal.set(HOUR_OF_DAY, 23);
