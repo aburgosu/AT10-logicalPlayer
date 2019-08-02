74dce09d03e02d32bd48cc85d0c1a53d0ac20eb6
aburgosu <49449153+aburgosu@users.noreply.github.com>
2019-08-02 15:04:10 -0400

    Update ServiceConnectionTest.java

--- src/test/java/com/fundation/logic/model/ServiceConnectionTest.java
+++ src/test/java/com/fundation/logic/model/ServiceConnectionTest.java
@@ -1,8 +1,7 @@
 package com.fundation.logic.model;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ServiceConnectionTest {
 
